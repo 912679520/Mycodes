23b995377a0be97940eca22948106c67725a5b38
912679520 <72492583+912679520@users.noreply.github.com>
2020-11-22 20:17:24 +0800

    快排非递归实现

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,5 +1,6 @@
 
-
+//#include"../顺序栈/stack.cpp"
+#include"../顺序栈/stack.h"
 #include"sort.h"
 
 int OneQuick(int* arr, int start, int end)
@@ -29,6 +30,45 @@ void Quick(int* arr, int start, int end)
 	Quick(arr, start, mid - 1);
 	Quick(arr, mid + 1, end);
 }
+
+//非递归实现
+/*
+	空间复杂度：由栈的使用空间决定——O（log（n））
+	如果在一个工程引用其他工程的代码
+	1、在使用另一个工程的代码的源文件中直接使用include".cpp"文件   （一般不使用）
+	2、设置工程属性，在工程中加入引用其他工程，同时将要引入的工程的配置类型改为动态库和静态库
+*/
+void Quick2(int* arr, int start, int end)
+{
+	stack st;
+	InitStack(&st);
+	ElemType val = {start, end};
+	Push(&st, val); //将整个数据段的起始位置和结束位置Push到栈中
+
+	while (!Empty(&st))
+	{
+		ElemType data;
+		Top(&st, &data);
+		Pop(&st);
+
+		int mid = OneQuick(arr, data.start, data.end);
+		if(mid - data.start > 1)
+		{
+			val.start = data.start;
+			val.end = mid - 1;
+			Push(&st, val);  //mid这个基准数据左边有超过一个的数据
+		}
+		if (data.end - mid > 1)
+		{
+			val.start = mid + 1;
+			val.end = data.end;
+			Push(&st, val);   //mid这个基准数据右边有超过一个数据
+		}
+	}
+
+	Destroy(&st);
+}
+
 
 void QuickSort(int* arr, int len)
 {
