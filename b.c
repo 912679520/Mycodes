d5e2320954d1f9fe6fa9bd0aa458ec91637b41e1
912679520 <72492583+912679520@users.noreply.github.com>
2020-12-07 12:58:03 +0800

    信号量

---
+++ b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <assert.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<wait.h>
+#include"sem.h"
+
+//b.c
+int main()
+{
+    sem_init();
+    for(int i =0;i < 5; i++)
+    {
+        sem_p();
+        printf("b");
+        fflush(stdout);
+        int n = rand() % 3;
+        sleep(n);
+        printf("b");
+        fflush(stdout);
+        sem_v();
+        n = rand() % 3;
+        sleep(n);
+    }
+}
