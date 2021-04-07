26f3a003a7c80f153e9def3df375746beb93d345
912679520 <72492583+912679520@users.noreply.github.com>
2021-04-07 15:03:32 +0800

    dffer 17.cpp

---
+++ dffer 17.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+
+/*
+class Solution {
+public:
+    vector<int> printNumbers(int n) 
+    {
+        const int num = pow(10, n);
+        vector<int> arr(num - 1);
+        for (int i = 0; i < num - 1; i++)
+        {
+            arr[i] = i + 1;
+        }
+        return arr;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    Solution a;
+    vector<int> arr = a.printNumbers(n);
+    for (int i = 0; i < arr.size();i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}*/
